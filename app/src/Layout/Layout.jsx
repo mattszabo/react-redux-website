fa8e007d643fa54bafd0deb36ebb9f455c167ea1
Matt Szabo <wekilledit@users.noreply.github.com>
2016-06-10 20:31:08 +1000

    set Home to home page and removed navbar from app

--- app/src/Layout/Layout.jsx
+++ app/src/Layout/Layout.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import NavbarApp from '../Navbar/components/NavbarApp'
+
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div className='layout'>
-        <NavbarApp />
+        <div className='navbar'>
+          <ul>
+            <li>Home</li>
+            <li>Todo</li>
+            <li>About</li>
+          </ul>
+        </div>
         <div className='content'>
           {this.props.children}
         </div>
