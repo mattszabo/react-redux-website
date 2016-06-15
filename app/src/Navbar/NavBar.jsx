06401e17383c5143ecc765cf9acae2a09027bd35
Matt Szabo <wekilledit@users.noreply.github.com>
2016-06-15 22:28:47 +1000

    made a NavBar component and modified stylings

--- app/src/Navbar/NavBar.jsx
+++ app/src/Navbar/NavBar.jsx
@@ -1,38 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import NavBarData from './NavBarData';
-import NavBarItem from './NavBarItem';
-
-// Renders the navbar items and also acts as the controller by handling
-// navbar item clicks that are passed up by the NavBarItem class
-export default class NavBar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedItem: 0,
-      navBarList: NavBarData.getNavBarList()
-    }
-  }
-
-  _updateItemSelection(itemId) {
-    this.setState({selectedItem: itemId});
-  }
-
-  render() {
-    return(
-      <div>
-        <ul>
-          {this.state.navBarList.map((item) =>
-            <NavBarItem
-              key = {item.id}
-              url = {item.url}
-              text = {item.text}
-              onClick = {this._updateItemSelection.bind(this, item.id)}
-              isSelected={(this.state.selectedItem === item.id)}
-            />
-          )}
-        </ul>
-      </div>
-    );
-  }
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Counter</li>
+        <li>CounterEven</li>
+      </ul>
+    </nav>
+  )
 }
+
+export default Navbar
